ae38a554463c3c30415bfa133e1b61a2b282d83f
MaiyalaganS21 <114276487+MaiyalaganS21@users.noreply.github.com>
2022-11-30 04:37:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+setTimeout(mycar, 3000);
+
+function mycar() {
+  document.getElementById("run").innerHTML = "Good morning beautiful";
+}
+
+setTimeout(mycar1, 6000);
+
+function mycar1() {
+  document.getElementById("run1").innerHTML = "I love You so much sweetheart!!";
+}
